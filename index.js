a3e5ce3b3c0150116b53d2f4ff49626097b9250a
XuanzhaoS <t3zhxu00@students.oamk.fi>
2024-10-07 11:29:25 +0300

    Bundle1 Assignment Done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World!<br> This is Bundle 1 Assignment.');
 });
 
 app.listen(port, () => {
